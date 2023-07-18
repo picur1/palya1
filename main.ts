041e07fc6b5a2bc9a65af4e8e5fa431b22e092d2
picur1 <133989406+picur1@users.noreply.github.com>
2023-07-18 12:58:17 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("N", 15 * 5)
+    for (let index = 0; index < 4; index++) {
+        radio.sendValue("N", 33 * 5)
+        basic.pause(2000)
+        radio.sendValue("R", 78)
+        basic.pause(2000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+	
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("R", 86)
+	
 })
 radio.setGroup(1)
 basic.forever(function () {
